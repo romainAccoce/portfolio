351afcdd289f3b5c83c34037ae407444a85fcf3f
romainAccoce <romainaccoce@gmail.com>
2022-01-28 10:08:05 +0100

    add html home structure

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,15 @@
 import './home.scss';
 
+import picture from '../../assets/images/photo1.png';
+
 const Home = () => {
   return (
     <div className="home">
-      
+        <div className='homme__image-container'>
+            <img className='home__image-container__image' src={picture}/>
+        </div>
+        <p className='home__catch-phrase'>Hi, I'm Romain, Front-End web developer</p>
+        <div className='home__contact-button'>contact me</div>
     </div>
   );
 }

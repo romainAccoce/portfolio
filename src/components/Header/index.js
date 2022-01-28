b16d2a6ea572275382b1c4a1b4f8fd4608926cb2
romainAccoce <romainaccoce@gmail.com>
2022-01-28 17:03:59 +0100

    add header html structure

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,16 @@ import './header.scss';
 const Header = () => {
   return (
     <div className="header">
-      header
+        <div className='header__logo'>RA</div>
+        <nav className='header__nav'>
+            <ul className='header__nav__list'>
+                <li className='header__nav__list__item'>About</li>
+                <li className='header__nav__list__item'>Skills</li>
+                <li className='header__nav__list__item'>Projects</li>
+                <li className='header__nav__list__item'>Contact</li>
+            </ul>
+        </nav>
+        <div className='header__icons-container'></div>
     </div>
   );
 }

eb3e27a719564bca835832dba7cc7304489ed3f0
romainAccoce <romainaccoce@gmail.com>
2022-01-28 17:59:21 +0100

    nav html fixed

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,16 +3,16 @@ import './header.scss';
 const Header = () => {
   return (
     <div className="header">
-        <div className='header__logo'>RA</div>
         <nav className='header__nav'>
+            <div className='header__nav__logo'>RA</div>
             <ul className='header__nav__list'>
                 <li className='header__nav__list__item'>About</li>
                 <li className='header__nav__list__item'>Skills</li>
                 <li className='header__nav__list__item'>Projects</li>
                 <li className='header__nav__list__item'>Contact</li>
             </ul>
+            <div className='header__nav__icons-container'>Social media icons</div>
         </nav>
-        <div className='header__icons-container'>Social media icons</div>
     </div>
   );
 }

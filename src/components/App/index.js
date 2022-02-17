6988d24df18ff2433d6cfb813bea7f94220363d3
romainAccoce <romainaccoce@gmail.com>
2022-02-17 10:04:41 +0100

    setup router

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,14 +1,26 @@
+import { Routes, Route } from "react-router-dom";
+
 import './app.scss';
 
 import Header from '../Header';
 import Home from '../Home';
+import About from "../About";
+import Skills from "../Skills";
+import Projects from "../Projects";
+import Contact from "../Contact";
 import Footer from '../Footer';
 
 const App = () => {
   return (
     <div className="app">
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
       <Footer />
     </div>
   );
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,22 +1,22 @@
 import './header.scss';
+import { Link } from 'react-router-dom';
 
 import {
   faLinkedinIn, faInstagram, faTwitter, faGithub,
 } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-
 const Header = () => {
   return (
     <div className="header">
         <nav className='header__nav'>
-            <div className='header__nav__logo'>RA</div>
-            <ul className='header__nav__list'>
-                <li className='header__nav__list__item'>About</li>
-                <li className='header__nav__list__item'>Skills</li>
-                <li className='header__nav__list__item'>Projects</li>
-                <li className='header__nav__list__item'>Contact</li>
-            </ul>
+            <Link to='/' className='header__nav__logo highlight-text'>RA</Link>
+            <nav className='header__nav__list'>
+                <Link to='/about' className='header__nav__list__item'>About</Link>
+                <Link to='/skills' className='header__nav__list__item'>Skills</Link>
+                <Link to='/projects' className='header__nav__list__item'>Projects</Link>
+                <Link to='/contact' className='header__nav__list__item'>Contact</Link>
+            </nav>
             <div className='header__nav__icons-container'>
                 <a className='header__nav__icons-container__icon' href="https://github.com/romainAccoce/" target="_blank" rel="noreferrer">
                     <FontAwesomeIcon icon={faGithub} />

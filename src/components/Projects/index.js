fc0ae108b332422188aa4d2610e7ea5963f6c2a6
romainAccoce <romainaccoce@gmail.com>
2022-04-06 11:06:49 +0200

    import projects datas in projects component

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,9 +1,20 @@
 import './projects.scss';
+import { projectsDatas } from '../../selectors';
 
 const Projects = () => {
     return (
         <div className='projects'>
-            
+            {
+                projectsDatas.map((project) => (
+                    <div className='project-container' key={project.url}>
+                        <p>{project.name}</p>
+                        <p>{project.description}</p>
+                        <p>{project.url}</p>
+                        <p>{project.githubLink}</p>
+                    </div>
+                    ),
+                )
+            }
         </div>
     );
 };

89de23c34b39957ac04b8264ba62fa655bd25f71
romainAccoce <romainaccoce@gmail.com>
2022-04-06 10:51:21 +0200

    add projects datas

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -2,15 +2,15 @@ export const projectsDatas = [
     {
         name: 'DEV Interest',
         description: 'A web platform to share and organize ressources around development',
-        techs: ['javascript','react', 'readux', 'sass', 'opengraph'],
+        techs: ['javascript','react', 'redux', 'sass', 'opengraph'],
         url: 'https://devinterest.surge.sh/',
         githubLink: '',
     },
     {
-        name: '',
-        description: '',
-        techs: [],
-        url: '',
-        githubLink: '',
+        name: 'Rock Paper Scissors',
+        description: 'Play the classic game with original rules or switch to "Lizzard Spock" mode to spice it up',
+        techs: ['javascript','react', 'redux', 'sass'],
+        url: 'https://romacc-rock-paper-scissors.surge.sh/',
+        githubLink: 'https://github.com/romainAccoce/rock-paper-scissors',
     },
 ]

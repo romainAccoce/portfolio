e5922312712597aa0a6da97fc8ad2fdf42e307af
romainAccoce <romainaccoce@gmail.com>
2022-04-11 18:41:15 +0200

    home layout optimisation

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -8,14 +8,13 @@ const Home = () => {
 
   return (
     <div className="home">
-        <img className='home__image' src={picture}/>
         <div className='home__text-container'>
             <p className='home__text-container__text'>Hi,</p>
             <p className='home__text-container__text'>I'm <strong className='highlight-text'>Romain</strong>,</p> 
             <p className='home__text-container__text'>Front End web developer</p>
         </div>
         <div className='home__techs-container'>
-            <p className='home__techs-container__tech highlight-text'>#ReactWebDeveloper</p>
+            <p className='home__techs-container__tech highlight-text'>#ReactDeveloper</p>
         </div>
     </div>
   );
--- src/selectors/index.js
+++ src/selectors/index.js
@@ -9,7 +9,7 @@ export const projectsDatas = [
     },
     {
         name: 'Rock Paper Scissors',
-        description: 'Play the classic game with original rules or switch to "Lizzard Spock" mode to spice it up',
+        description: 'Play the classic game with original rules or switch to "Lizzard Spock" mode to spice it up.',
         techs: ['React', 'Redux', 'Sass'],
         url: 'https://romacc-rock-paper-scissors.surge.sh/',
         githubLink: 'https://github.com/romainAccoce/rock-paper-scissors',

8cd8475dbe22b6eeef05a745700422615f11a1cf
romainAccoce <romainaccoce@gmail.com>
2022-04-14 15:46:04 +0200

    add animations on home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,12 +7,14 @@ const Home = () => {
   return (
     <div className="home">
         <div className='home__text-container'>
-            <p className='home__text-container__text'>Hi,</p>
-            <p className='home__text-container__text'>I'm <strong className='highlight-text'>Romain</strong>,</p> 
-            <p className='home__text-container__text'>Front End web developer</p>
+            <p className='home__text-container__line'>Hi,</p>
+            <p className='home__text-container__line'>I'm <strong className='highlight-text'>Romain</strong>,</p> 
+            <p className='home__text-container__line'>Web developer</p>
+            <p className='home__text-container__tech'>Front End developer / React expert</p>
         </div>
-        <p className='home__tech-specification'>#ReactDeveloper</p>
-        <Button className='home__button' name='Contact me!' size={'large'}/>
+        <div className='home__button'>
+          <Button name='Contact me!' size={'large'}/>
+        </div>
     </div>
   );
 }

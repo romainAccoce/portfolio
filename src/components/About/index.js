4a9fad4a5185811e2bbf2fc45b07479220b30da9
romainAccoce <romainaccoce@gmail.com>
2022-05-10 17:06:18 +0200

    add about page style and animations

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,8 +5,10 @@ const About = () => {
     return (
         <div className='about'>
             <div className='about__content'>
-                <h2 className='about__content__title'>About me</h2>
-                <p className='about__content__text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa sunt quae exercitationem non! Qui natus alias earum dolores nisi doloremque, blanditiis quae, ipsum aperiam assumenda aliquid omnis pariatur, aspernatur quasi minima? Facere earum excepturi laboriosam suscipit doloremque, quam dignissimos? Sed ratione suscipit voluptatum sunt, perspiciatis mollitia id repellat incidunt. Laborum!</p>
+                <h2 className='about__content__title'>My journey so far</h2>
+                <p className='about__content__text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa sunt quae exercitationem non! Qui natus alias earum dolores nisi doloremque, blanditiis quae, ipsum aperiam assumenda aliquid omnis pariatur, aspernatur quasi minima?</p>
+                <p className='about__content__text'>Facere earum excepturi laboriosam suscipit doloremque, quam dignissimos? Sed ratione suscipit voluptatum sunt, perspiciatis mollitia id repellat incidunt. Laborum!</p>
+                <p className='about__content__text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa sunt quae exercitationem non! Qui natus alias earum dolores nisi doloremque, blanditiis quae, ipsum aperiam assumenda aliquid omnis pariatur, aspernatur quasi minima?</p>
             </div>
             <img className='about__image' src={Image}/>
         </div>

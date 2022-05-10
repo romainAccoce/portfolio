8958a5d46e484471593281de1c458949f679de32
romainAccoce <romainaccoce@gmail.com>
2022-05-10 11:16:58 +0200

    add about page elements

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,8 +1,13 @@
 import './about.scss';
+import Image from '../../assets/images/photo1.png';
 
 const About = () => {
     return (
-        <div>About</div>
+        <div className='about'>
+            <div className='about__title'>About me</div>
+            <div className='about__text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa sunt quae exercitationem non! Qui natus alias earum dolores nisi doloremque, blanditiis quae, ipsum aperiam assumenda aliquid omnis pariatur, aspernatur quasi minima? Facere earum excepturi laboriosam suscipit doloremque, quam dignissimos? Sed ratione suscipit voluptatum sunt, perspiciatis mollitia id repellat incidunt. Laborum!</div>
+            <img className='about__image' src={Image}/>
+        </div>
     );
 };
 

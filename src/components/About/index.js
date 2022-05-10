e61dc99c0ce27a952fc412ce0fb304db48de5557
romainAccoce <romainaccoce@gmail.com>
2022-05-10 11:52:28 +0200

    add titles factorisation and fixed home page structure

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,8 +4,10 @@ import Image from '../../assets/images/photo1.png';
 const About = () => {
     return (
         <div className='about'>
-            <div className='about__title'>About me</div>
-            <div className='about__text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa sunt quae exercitationem non! Qui natus alias earum dolores nisi doloremque, blanditiis quae, ipsum aperiam assumenda aliquid omnis pariatur, aspernatur quasi minima? Facere earum excepturi laboriosam suscipit doloremque, quam dignissimos? Sed ratione suscipit voluptatum sunt, perspiciatis mollitia id repellat incidunt. Laborum!</div>
+            <div className='about__content'>
+                <h2 className='about__content__title'>About me</h2>
+                <p className='about__content__text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa sunt quae exercitationem non! Qui natus alias earum dolores nisi doloremque, blanditiis quae, ipsum aperiam assumenda aliquid omnis pariatur, aspernatur quasi minima? Facere earum excepturi laboriosam suscipit doloremque, quam dignissimos? Sed ratione suscipit voluptatum sunt, perspiciatis mollitia id repellat incidunt. Laborum!</p>
+            </div>
             <img className='about__image' src={Image}/>
         </div>
     );
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,11 +7,11 @@ const Home = () => {
   return (
     <div className="home">
         <div className='home__text-container'>
-            <h2 className='home__text-container__title'>
+            <div className='home__text-container__title'>
               Hi,<br/>
               I'm <strong className='highlight-text'>Romain</strong>,<br/>
               web developer
-            </h2>
+            </div>
             <p className='home__text-container__tech'>Front End developer / React expert</p>
         </div>
         <div className='home__button'>

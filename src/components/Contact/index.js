6033621d4b5299885cf539b9c8428a8fcaa57ca0
romainAccoce <romainaccoce@gmail.com>
2022-05-12 12:28:58 +0200

    add contact page content structure

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,6 +4,9 @@ import Map from '../Map';
 const Contact = () => {
     return (
         <div className='contact'>
+            <div className='contact__content'>
+                <h2 className='contact__content__title'>Contact me</h2>
+            </div>
             <div className='contact__map'>
                 <Map />
             </div>

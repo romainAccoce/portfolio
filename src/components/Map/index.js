1e60198acb0ac056be434bcc8b1853597671a701
romainAccoce <romainaccoce@gmail.com>
2022-05-12 13:04:02 +0200

    setup MapContainer

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -13,7 +13,7 @@ const Map = () => {
         <MapContainer className='map-container' center={coordinates} zoom={10} scrollWheelZoom={true}>
             <TileLayer
                 attribution='<a href="https://carto.com/" target="_blank">&copy; CARTO</a> <a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
-                url="https://api.maptiler.com/maps/voyager/256/{z}/{x}/{y}.png?key=yu6G433GVhDlDGWmQSdQ"
+                url="https://api.maptiler.com/maps/pastel/256/{z}/{x}/{y}.png?key=yu6G433GVhDlDGWmQSdQ"
             />
             <Marker position={coordinates} />
         </MapContainer>

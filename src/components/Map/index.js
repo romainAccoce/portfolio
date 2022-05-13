dca2f1b794cd642057b51d272c929b5389174a93
romainAccoce <romainaccoce@gmail.com>
2022-05-13 14:47:45 +0200

    remove zoom control from map

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -10,7 +10,7 @@ const coordinates = [48.814552763145905, 2.406253325234966];
 
 const Map = () => {
     return (
-        <MapContainer className='map-container' center={coordinates} zoom={10} scrollWheelZoom={true}>
+        <MapContainer className='map-container' center={coordinates} zoom={10} zoomControl={false} scrollWheelZoom={true}>
             <TileLayer
                 attribution='<a href="https://carto.com/" target="_blank">&copy; CARTO</a> <a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
                 url="https://api.maptiler.com/maps/pastel/256/{z}/{x}/{y}.png?key=yu6G433GVhDlDGWmQSdQ"

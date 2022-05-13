75ab077f3e4346ceb57cf32056fcd7503cd396e1
romainAccoce <romainaccoce@gmail.com>
2022-05-13 14:43:04 +0200

    add input border on focus

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -7,7 +7,7 @@ const Contact = () => {
         <div className='contact'>
             <div className='contact__content'>
                 <h2 className='contact__content__title'>Contact me</h2>
-                <p className='contact__content__text'>I’m interested in freelance opportunities, especially ambitious or large projects. However, if you have other request or question, don’t hesitate to use the form. </p>
+                <p className='contact__content__text'>I’m interested in freelance opportunities, if you have any request or question, don’t hesitate to use the form. </p>
                 <ContactForm />
             </div>
             <div className='contact__map'>

3b786088b2a0ceb590cc53aae78eac39b4e1d721
romainAccoce <romainaccoce@gmail.com>
2022-05-13 17:03:32 +0200

    add contact page animation

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -15,7 +15,9 @@ const Map = () => {
                 attribution='<a href="https://carto.com/" target="_blank">&copy; CARTO</a> <a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
                 url="https://api.maptiler.com/maps/pastel/256/{z}/{x}/{y}.png?key=yu6G433GVhDlDGWmQSdQ"
             />
-            <Marker position={coordinates} />
+            <div className='map-container__marker'>
+                <Marker  position={coordinates} />
+            </div>
         </MapContainer>
     );
 };

d033124bbcca9852b90eb786f0fb9a194d50e11d
romainAccoce <romainaccoce@gmail.com>
2022-06-21 15:09:56 +0200

    add aos animation to map

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -10,16 +10,18 @@ const coordinates = [48.814552763145905, 2.406253325234966];
 
 const Map = () => {
     return (
-        <MapContainer className='map-container' center={coordinates} zoom={10} zoomControl={false} scrollWheelZoom={false}>
-            <TileLayer
-                attribution='<a href="https://carto.com/" target="_blank">&copy; CARTO</a> <a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
-                url="https://api.maptiler.com/maps/pastel/256/{z}/{x}/{y}.png?key=yu6G433GVhDlDGWmQSdQ"
-            />
-            <ZoomControl position='bottomright' />
-            <div className='map-container__marker'>
-                <Marker  position={coordinates} />
-            </div>
-        </MapContainer>
+        <div data-aos="zoom-in-up" className='map'>
+            <MapContainer className='map__container' center={coordinates} zoom={10} zoomControl={false} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='<a href="https://carto.com/" target="_blank">&copy; CARTO</a> <a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+                    url="https://api.maptiler.com/maps/pastel/256/{z}/{x}/{y}.png?key=yu6G433GVhDlDGWmQSdQ"
+                />
+                <ZoomControl position='bottomright' />
+                <div className='map__container__marker'>
+                    <Marker  position={coordinates} />
+                </div>
+            </MapContainer>
+        </div>
     );
 };
 

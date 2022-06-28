c924f41b1a3712e3f89cfa8dce570c6fd2116b25
romainAccoce <romainaccoce@gmail.com>
2022-06-28 12:16:09 +0200

    home page layout optimisation

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,22 +5,22 @@ const Home = () => {
 
   return (
     <div className="home" id='home'>
-        <div className='home__text-container'>
-            <div className='home__text-container__title'>
-              Hi,<br/>
-              I'm <strong className='highlight-text'>Romain</strong>,<br/>
-              web developer
-            </div>
-            <p className='home__text-container__tech'>Front End developer / React expert</p>
+      <div className='home__content'>
+        <div className='home__content__title'>
+          Hi,<br/>
+          I'm <strong className='highlight-text'>Romain</strong>,<br/>
+          web developer
         </div>
+        <p className='home__content__tech'>Front End developer / React expert</p>
         {/* Due to the presence of the Link component I cannot use the generic button component, the classname 'button' apply the generic component style anyway*/}
-        <div className='button home__button'>
+        <div className='home__content__button button'>
           <Link
               to="contact"
               smooth={true}
               duration={500}
           >Contact me</Link>
         </div>
+      </div>
     </div>
   );
 }

12a6dca4d353b2f1fa36187bb1169d6e033e2b92
romainAccoce <romainaccoce@gmail.com>
2022-07-04 14:35:12 +0200

    fixed form animations

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,7 +7,7 @@ const About = () => {
         <div className='about'id='about'>
             <div className='about__content' >
                 <h2 data-aos="fade-right" className='about__content__title'>My journey so far</h2>
-                <p data-aos="fade-up" className='about__content__text'>After several years oparating in Human Ressources, my last experiences in tech oriented environnements, especially in talent aquisition management, made me want to be a part of projects developpement more concretely.</p>
+                <p data-aos="fade-up" className='about__content__text'>After several years operating in Human Ressources, my last experiences in tech oriented environnements, especially in talent aquisition management, made me want to be a part of projects development more concretely.</p>
                 <p data-aos="fade-up" className='about__content__text'>My natural taste and ability for solving problems and results optimisation pushed me to start a professional retraining in web development.</p>
                 <p data-aos="fade-up" className='about__content__text'>I started a six months fullstack Javascript developer formation in O'clock school, where I have learned the fondamentals of Javascript development on both client and server side before I chose the front end React specialisation. Today, I seek on every oportunity to sharpen my skills by team collaboration on ambitious projects.</p>
             </div>
--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -25,52 +25,49 @@ const ContactForm = () => {
   };
 
   return (
-    <form className='form' ref={form} onSubmit={sendEmail}>
+    <form 
+      className='form' 
+      ref={form}
+      onSubmit={sendEmail}
+      data-aos="fade-up"
+      data-aos-delay={100}
+    >
       <input 
         className='form__input form__input__name'
         value={name}
-        data-aos="fade-up"
-        data-aos-delay={100}
         type="text"
         name="user_name"
         placeholder='Name'
         onChange={(event) => dispatch(changeFormField(event.target.value, 'name'))}
         required
-        />
+      />
       <input
         className='form__input form__input__email'
         value={email}
-        data-aos="fade-up"
-        data-aos-delay={200}
         type="email"
         name="user_email"
         placeholder='Email'
         onChange={(event) => dispatch(changeFormField(event.target.value, 'email'))}
         required
-        />
+      />
       <input
-      className='form__input form__input__subject'
-      value={subject}
-      data-aos="fade-up"
-      data-aos-delay={300}
-      type="text"
-      name="subject"
-      placeholder='Subject'
-      onChange={(event) => dispatch(changeFormField(event.target.value, 'subject'))}
-
-      required
+        className='form__input form__input__subject'
+        value={subject}
+        type="text"
+        name="subject"
+        placeholder='Subject'
+        onChange={(event) => dispatch(changeFormField(event.target.value, 'subject'))}
+        required
       />
       <textarea
-      className='form__input form__input__message'
-      value={message}
-      data-aos="fade-up"
-      data-aos-delay={400}
-      name="message"
-      placeholder='Message'
-      onChange={(event) => dispatch(changeFormField(event.target.value, 'message'))}
-      required
+        className='form__input form__input__message'
+        value={message}
+        name="message"
+        placeholder='Message'
+        onChange={(event) => dispatch(changeFormField(event.target.value, 'message'))}
+        required
       />
-      <div data-aos="fade-up" data-aos-delay={500}>
+      <div>
         <input className='form__submit button' type="submit" value="Send message" />
       </div>
     </form>

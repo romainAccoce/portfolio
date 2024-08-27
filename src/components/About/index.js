f9b320ed749897dc189dbcb4571d560a09f162fe
romainAccoce <romainaccoce@gmail.com>
2024-08-27 17:48:32 -0400

    Lint fix

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,19 +1,35 @@
-import './about.scss';
-import Image from '../../assets/images/photo5.jpg';
+import "./about.scss";
+import Image from "../../assets/images/photo5.jpg";
 
 const About = () => {
-
-    return (
-        <div className='about'id='about'>
-            <div className='about__content' >
-                <h2 data-aos="fade-right" className='about__content__title'>My journey so far</h2>
-                <p data-aos="fade-up" className='about__content__text'>After several years operating in Human Ressources, my last experiences in tech oriented environnements, especially in talent aquisition management, made me want to be a part of projects development more concretely.</p>
-                <p data-aos="fade-up" className='about__content__text'>My natural taste and ability for solving problems and results optimisation pushed me to start a professional retraining in web development.</p>
-                <p data-aos="fade-up" className='about__content__text'>I started a six months fullstack Javascript developer formation in O'clock school, where I have learned the fondamentals of Javascript development on both client and server side before I chose the front end React specialisation. Today, I seek on every oportunity to sharpen my skills by team collaboration on ambitious projects.</p>
-            </div>
-            <img data-aos="flip-right" className='about__image' src={Image}/>
-        </div>
-    );
+  return (
+    <div className="about" id="about">
+      <div className="about__content">
+        <h2 data-aos="fade-right" className="about__content__title">
+          My journey so far
+        </h2>
+        <p data-aos="fade-up" className="about__content__text">
+          After several years operating in Human Ressources, my last experiences
+          in tech oriented environnements, especially in talent aquisition
+          management, made me want to be a part of projects development more
+          concretely.
+        </p>
+        <p data-aos="fade-up" className="about__content__text">
+          My natural taste and ability for solving problems and results
+          optimisation pushed me to start a professional retraining in web
+          development.
+        </p>
+        <p data-aos="fade-up" className="about__content__text">
+          I started a six months fullstack Javascript developer formation in
+          O'clock school, where I have learned the fondamentals of Javascript
+          development on both client and server side before I chose the front
+          end React specialisation. Today, I seek on every oportunity to sharpen
+          my skills by team collaboration on ambitious projects.
+        </p>
+      </div>
+      <img data-aos="flip-right" className="about__image" src={Image} alt="" />
+    </div>
+  );
 };
 
 export default About;
--- src/components/BackToTopButton/index.js
+++ src/components/BackToTopButton/index.js
@@ -1,32 +1,36 @@
-import './back-to-top-button.scss';
-import { useDispatch, useSelector } from 'react-redux';
-import { scrollToTop } from '../../selectors';
-import { useEffect } from 'react';
-import { setScrollToTopButton } from '../../actions/displayOptions';
-import { FaAngleDoubleUp } from 'react-icons/fa'
+import "./back-to-top-button.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { scrollToTop } from "../../selectors";
+import { useEffect } from "react";
+import { setScrollToTopButton } from "../../actions/displayOptions";
+import { FaAngleDoubleUp } from "react-icons/fa";
 
 const BackToTopButton = () => {
-    const dispatch = useDispatch();
-    const { scrollToTopButton } = useSelector((state) => state.displayOptions);
+  const dispatch = useDispatch();
+  const { scrollToTopButton } = useSelector((state) => state.displayOptions);
 
-    useEffect(() => {
-        window.addEventListener('scroll', () => {
-            if (window.scrollY > 300) {
-                dispatch(setScrollToTopButton(true));
-            } else {
-                dispatch(setScrollToTopButton(false));
-            }
-        });
-    }, []);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 300) {
+        dispatch(setScrollToTopButton(true));
+      } else {
+        dispatch(setScrollToTopButton(false));
+      }
+    });
+  }, [window]);
 
-
-    return (
-        <div onClick={ () => scrollToTop()} className={ scrollToTopButton ? 'scroll' : 'scroll scroll--reverse' }>
-            <div className={ scrollToTopButton ? 'scroll__to-top' : 'scroll__to-bottom' }  >
-                <FaAngleDoubleUp />
-            </div>
-        </div>
-    );
+  return (
+    <div
+      onClick={() => scrollToTop()}
+      className={scrollToTopButton ? "scroll" : "scroll scroll--reverse"}
+    >
+      <div
+        className={scrollToTopButton ? "scroll__to-top" : "scroll__to-bottom"}
+      >
+        <FaAngleDoubleUp />
+      </div>
+    </div>
+  );
 };
 
 export default BackToTopButton;

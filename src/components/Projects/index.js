465351dbda773a1b533a697a7ce2ad7f3518425d
romainAccoce <romainaccoce@gmail.com>
2024-08-27 17:55:20 -0400

    Revert button changes

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -36,54 +36,52 @@ const Projects = () => {
         My work
       </h2>
       <div className="projects__container">
-        {projects.length > 0 ? (
-          projects.map((project, index) => (
-            <div
-              data-aos="zoom-in-up"
-              data-aos-delay={300 * index}
-              className="projects__container__project-card"
-              key={project.attributes.name}
-              onMouseEnter={() =>
-                dispatch(setCurrentCard(project.attributes.name))
+        {projects.map((project, index) => (
+          <div
+            data-aos="zoom-in-up"
+            data-aos-delay={300 * index}
+            className="projects__container__project-card"
+            key={project.attributes.name}
+            onMouseEnter={() =>
+              dispatch(setCurrentCard(project.attributes.name))
+            }
+            onMouseLeave={() => dispatch(setCurrentCard(""))}
+            onTouchStart={() => handleTouchStart(project)}
+            onTouchMove={() => handleTouchMove(project)}
+          >
+            <img
+              alt=""
+              className={
+                currentCard === project.attributes.name
+                  ? "projects__container__project-card__image projects__container__project-card__image--current"
+                  : "projects__container__project-card__image"
               }
-              onMouseLeave={() => dispatch(setCurrentCard(""))}
-              onTouchStart={() => handleTouchStart(project)}
-              onTouchMove={() => handleTouchMove(project)}
-            >
-              <img
-                alt=""
-                className={
-                  currentCard === project.attributes.name
-                    ? "projects__container__project-card__image projects__container__project-card__image--current"
-                    : "projects__container__project-card__image"
-                }
-                src={project.attributes.image.data.attributes.url}
-              />
-              <div className="projects__container__project-card__content">
-                <h3 className="projects__container__project-card__content__title">
-                  {project.attributes.name}
-                </h3>
-                <div className="projects__container__project-card__content__techs">
-                  {project.attributes.techs.front.map((tech) => (
-                    <Tag tech={tech} key={tech} />
-                  ))}
-                </div>
-                <p className="projects__container__project-card__content__description">
-                  {project.attributes.description}
-                </p>
-                <div className="projects__container__project-card__content__links">
-                  <Button name="Go to website" link={project.attributes.url} />
-                  <Button
-                    name="Source code"
-                    link={project.attributes.githubLink}
-                  />
-                </div>
+              src={project.attributes.image.data.attributes.url}
+            />
+            <div className="projects__container__project-card__content">
+              <h3 className="projects__container__project-card__content__title">
+                {project.attributes.name}
+              </h3>
+              <div className="projects__container__project-card__content__techs">
+                {project.attributes.techs.front.map((tech) => (
+                  <Tag tech={tech} key={tech} />
+                ))}
+              </div>
+              <p className="projects__container__project-card__content__description">
+                {project.attributes.description}
+              </p>
+              <div className="projects__container__project-card__content__links">
+                <Button name="Go to website" link={project.attributes.url} />
+                <a
+                  className="projects__container__project-card__content__links__github"
+                  href={project.attributes.githubLink}
+                >
+                  Source code
+                </a>
               </div>
             </div>
-          ))
-        ) : (
-          <>No data</>
-        )}
+          </div>
+        ))}
       </div>
     </div>
   );
